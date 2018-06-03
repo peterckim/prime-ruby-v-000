f68e506463b18100009425a7dcab0b00429019d9
Peter Kim <peterckim96@gmail.com>
2018-06-03 00:30:24 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(input)
+  list = []
+  count = 2
+  while count < input
+    list << count
+    count += 1
+  end
+  
+  list.each do |number|
+    return false if input % number == 0
+  end
+  return true
+end
